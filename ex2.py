4d33f7446dfc0ad26bb5f32a833f4ef4dc0fd760
samzhuwj <samzhuwj@gmail.com>
2017-12-23 19:04:48 +0800

    build a logistic regression model to predict whether a strudent gets admitted into a university

--- ex2.py
+++ ex2.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize as opt
 from plotData import *
+
 import costFunction as cf
 import plotDecisionBoundary as pdb
 import predict as predict
